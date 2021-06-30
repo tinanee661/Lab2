cee807b01cbd4f5f5d6cf2df712fe70b427db1e8
Arta Hyseni <ah45058@ubt-uni.net>
2021-06-30 09:36:27 +0200

    SendMail

---
+++ LaboratoriAnalizav/LaboratoriAnalizav/Controllers/SendMailDtoController.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReflectionIT.Mvc.Paging;
+using LaboratoriAnalizav.Data;
+using LaboratoriAnalizav.Models;
+
+namespace LaboratoriAnalizav.Controllers
+{
+
+    public class SendMailDtoController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public SendMailDtoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index(int page = 1)
+        {
+            var query = _context.SendMails.AsNoTracking().OrderBy(s => s.id);
+            var model = await PagingList.CreateAsync(query, 3, page);
+            return View(model);
+        }
+
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,LastName,Subject,Message,User")] SendMailDto sendMail)
+        {
+            System.Diagnostics.Debug.WriteLine("testet");
+            if (ModelState.IsValid)
+            {
+                _context.Add(sendMail);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Create));
+            }
+
+            return NotFound("Mail was not saved!!");
+
+        }
+
+    }
+}
---
+++ LaboratoriAnalizav/LaboratoriAnalizav/Models/SendMailDto.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaboratoriAnalizav.Models
+{
+    public class SendMailDto
+    {
+        public int id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+
+
+    }
+}
