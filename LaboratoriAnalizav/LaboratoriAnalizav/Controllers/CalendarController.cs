055bda80f3863913697da92c301a823b743b1cb1
Valentina Neziri <vn44713@ubt-uni.net>
2021-07-01 09:59:08 +0200

    Calendar Create

---
+++ LaboratoriAnalizav/LaboratoriAnalizav/Controllers/CalendarController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LaboratoriAnalizav.Models;
+using LaboratoriAnalizav.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LaboratoriAnalizav.Controllers
+{
+    public class CalendarController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        
+        public CalendarController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> GetEventData() {
+            List<Terminet> ObjEmp =await _context.Terminets.ToListAsync();
+            //return list as Json
+            return Json(ObjEmp);
+        }
+    }
+}
--- LaboratoriAnalizav/LaboratoriAnalizav/wwwroot/js/site.js
+++ LaboratoriAnalizav/LaboratoriAnalizav/wwwroot/js/site.js
@@ -1,4 +1,55 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    $('li.active').removeClass('active');
+    $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
+});
 
-// Write your JavaScript code.
+
+$(document).ready(function () {
+    $("#myInput").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#myTable tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
+
+$("#create-form").submit(function (e) {
+    e.preventDefault();
+    console.log("testtt");
+    let name = document.getElementById("fname").value;
+    let lastname = document.getElementById("lname").value;
+    let subject = document.getElementById("subject").value;
+    let message = document.getElementById("Message").value;
+    let user = "56ec1cf8-76bd-44a1-b3b1-f659710eac59";
+    var form = $('#create-form');
+    var token = $('input[name="__RequestVerificationToken"]', form).val();
+
+    let data = {
+        name,
+        lastname,
+        subject,
+        message,
+        user,
+        __RequestVerificationToken: token
+    };
+    
+    $.ajax({
+        type: "POST",
+        url: e.currentTarget.action,
+        data: data,
+        success: (e => {
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'We will reply to you as soon as we can',
+                showConfirmButton: false,
+                timer: 1500,
+
+            }).then(function () {
+                location.reload();
+            });
+        }),
+
+    })
+});
+
